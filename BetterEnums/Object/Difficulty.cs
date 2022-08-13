e12fe547c7bd4276b31205599be59c85bece8739
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2022-08-12 22:19:32 -0400

    Update Difficulty.cs

--- BetterEnums/Object/Difficulty.cs
+++ BetterEnums/Object/Difficulty.cs
@@ -2,10 +2,10 @@ namespace BetterEnums.Object;
 
 public class Difficulty
 {
-    public static Difficulty Easy { get; } = new Difficulty(1, "Just the Story!", "");
-    public static Difficulty Medium { get; } = new Difficulty(2, "Sword and Story!", "");
-    public static Difficulty Hard { get; } = new Difficulty(3, "Blood and Broken Bones!", "");
-    public static Difficulty VeryHard { get; } = new Difficulty(4, "Death March!", "");
+    public static Difficulty Easy { get; } = new Difficulty(1, "Just the Story!", "Enjoy a Smooth Ride Through the World");
+    public static Difficulty Normal { get; } = new Difficulty(2, "Sword and Story!", "You're Happy to be Challenged");
+    public static Difficulty Hard { get; } = new Difficulty(3, "Blood and Broken Bones!", "You're a Seasoned, Demanding Gamer");
+    public static Difficulty VeryHard { get; } = new Difficulty(4, "Death March!", "You're Truly Insane and Loving It");
 
     private Difficulty(int id, string title, string description)
     {
