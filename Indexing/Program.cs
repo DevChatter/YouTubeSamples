d32f47a173bc975899b067d142bc1ea1331cc0d8
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2022-08-15 17:12:03 -0400

    Code from Indexing video

---
+++ Indexing/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var letters = new [] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I' };
+
+Console.WriteLine(letters[letters.Length -1]);
+Console.WriteLine(letters[^1]);
+Console.WriteLine(letters[^3]);
+Console.WriteLine(letters[^5]);
+Console.WriteLine(letters[^7]);
+Console.WriteLine(letters[^9]);
+
+Console.WriteLine(letters[^5..^1]);
