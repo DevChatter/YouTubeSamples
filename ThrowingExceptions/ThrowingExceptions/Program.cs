e0962a58e4734d27f976af8258ace349b1f1c2ef
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2022-09-15 15:11:55 -0400

    Adding a rethrowing example

---
+++ ThrowingExceptions/ThrowingExceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ThrowingExceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ThrowingMethod();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private static void ThrowingMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
