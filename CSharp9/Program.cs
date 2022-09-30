5afd5dbae4d768809187a7214a72bf597bfad9c2
Brendan Enrick <2243498+benrick@users.noreply.github.com>
2022-09-30 15:08:46 -0400

    Adding first CSharp9 Sample

---
+++ CSharp9/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+Person me = new("Brendoneus", Color: "Yellow");
+
+if (me.Age is not null and >= 0 and < 150)
+{
+    WriteLine($"Age: {me.Age}");
+}
+
+
+public record Person
+    (string Name, string Color, int? Age = null)
+{
+}
